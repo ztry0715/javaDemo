c2aa38fc3eef240438f5d2c8c9de5f561f67fb49
zhouzhengzheng <zhouzhengzheng@lvmama.com>
2018-07-20 10:18:48 +0800

    测试

---
+++ gitDemo/src/com/zhou/gitdemo/Person.java
@@ -0,0 +1,44 @@
+package com.zhou.gitdemo;
+
+public class Person {
+	private Long id;
+	private String name;
+	private int age;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+
+	public Person(Long id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+
+}
---
+++ gitDemo/src/com/zhou/gitdemo/Test.java
@@ -0,0 +1,21 @@
+package com.zhou.gitdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		List<Person> list=new ArrayList<>();
+		list.add(new Person(1L,"aa",1));
+		list.add(new Person(2L,"aa",2));
+		list.add(new Person(3L,"aa",3));
+		list.add(new Person(4L,"aa",4));
+		list.add(new Person(5L,"aa",5));
+		list.add(new Person(6L,"aa",6));
+		list.add(new Person(7L,"aa",7));
+		list.add(new Person(7L,"aa",8));
+		list.add(new Person(9L,"aa",9));
+		list.stream().forEach(System.out::println);
+		
+	}
+}
